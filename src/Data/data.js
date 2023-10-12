cdd79cc7d1ed283030e7ea2d58ea22825e6162cc
unknown <amolspatil018@gmail.com>
2023-10-12 08:20:05 +0530

    first commit

---
+++ src/Data/data.js
@@ -0,0 +1,38 @@
+export const data = {
+  personal_info: {
+    first_name: "AMol",
+    last_name: "Patil",
+    email: "amolspatil018@gmail.com",
+    mobile: "+91 1234564556",
+    address: "10, Selvapuram, 3rd Street, Mannarai post",
+    city: "Tiruppur",
+    state: "Maharashtra",
+    postal_code: "641607",
+    objective:
+      "Take a tour of our Full Stack Data Web Development Course to learn about curriculum, assesments and projects designed to help you build a career in Full stack development. Take a tour of our Full stack development Course to learn about curriculum.",
+  },
+  work_experience: [
+    {
+      id: 1,
+      job_title: "Lerner",
+      organization_name: "Alma Better",
+      start_year: "2018",
+      end_year: "2022",
+    },
+    {
+      id: 2,
+      job_title: "Teacher",
+      organization_name: "Alma Better",
+      start_year: "2018",
+      end_year: "2022",
+    },
+  ],
+  education_details: {
+    type: "Graduation",
+    university: "NMU jalgaon univarsity",
+    degree: "B.Com",
+    start_year: "2018",
+    end_year: "2022",
+  },
+  key_skills: ["HTML", "CSS", "Javascript", "ReactJs", "NextJs"],
+};
---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,115 @@
+import { Link, Stack, Typography } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import EmailIcon from "@mui/icons-material/Email";
+import { Box } from "@mui/system";
+import Navbar from "../Components/Common/Navbar";
+
+export default function ButtonMUI() {
+  return (
+    <>
+      <Navbar />
+      <Stack p={{ xs: "15px", sm: "25px", md: "40px", lg: "60px " }}>
+        <h2 className="template-header-title">Resume Builder</h2>
+        <Stack
+          className="midContainer"
+          direction={{
+            xs: "column-reverse",
+            sm: "column-reverse",
+            md: "column-reverse",
+            lg: "row",
+          }}
+          spacing={{ xs: 1, sm: 2, md: 4 }}
+          mt="20px">
+          <Typography
+            sx={{
+              fontSize: {
+                xs: "13px",
+                sm: "15px",
+                md: "17px",
+                lg: "19px",
+              },
+              paddingRight: {
+                xs: "15px",
+                sm: "18px",
+                lg: "25px",
+              },
+              textAlign: "justify"
+            }}>
+
+            The online resume builder getting folks hired by BBC, Google, Apple, Tesla, and Airbnb.
+            You can pick one of our handcrafted resume templates above.
+            You can start building your resume in Minutes ,
+            using predefined sections approved by recruiters worldwide.
+            You can also customize it to your own needs and personality and hit 'Download'.
+            It's THAT easy to use, even if you've never made a resume in your life before!
+
+            <li>Build Your Resume Fast and Easy with Following steps </li>
+
+            <ol>1. Select Templete </ol>
+            <ol>2. Fill deatils </ol>
+            <ol>3. Your Resume is ready to download   </ol>
+
+          </Typography>
+          <Stack>
+            <img
+              src="https://www.visualcv.com/static/67fb7346de03f735bcf1b48751bb8b19/e3663/CV_Examples_2x.png"
+              alt="img"
+            />
+          </Stack>
+        </Stack>
+        <Box mt="25px">
+          <Typography
+            sx={{
+              fontSize: {
+                xs: "22px",
+                sm: "25px",
+                md: "27px",
+                lg: "30px",
+              },
+              fontWeight: "400",
+              color: "dark",
+            }}>
+            Share Resume Builder with your friends
+          </Typography>
+          <Box className="icons">
+            <Link href="https://facebook.com" >
+              <FacebookOutlinedIcon
+                sx={{ fontSize: "40px", paddingLeft: "15px", cursor: "pointer" }}
+                color="primary"
+              /></Link>
+
+            <Link href="https://linkedine.com" >
+              <LinkedInIcon
+                sx={{ fontSize: "40px", paddingLeft: "15px", cursor: "pointer" }}
+                color="primary"
+
+              ></LinkedInIcon></Link>
+
+
+            <Link href="www.whatsapp.com web" >
+              <WhatsAppIcon
+                sx={{ fontSize: "40px", paddingLeft: "15px", cursor: "pointer" }}
+                color="success"
+              ></WhatsAppIcon></Link>
+
+            <Link href="https://twitter.com/" >
+              <TwitterIcon
+                sx={{ fontSize: "40px", paddingLeft: "15px", cursor: "pointer" }}
+                color="info"
+              ></TwitterIcon></Link>
+
+
+            <Link href="https://email.com">
+              <EmailIcon
+                sx={{ fontSize: "40px", paddingLeft: "15px", cursor: "pointer" }}
+                color="error"
+              ></EmailIcon></Link>
+          </Box>
+        </Box>
+      </Stack>
+    </>
+  );
+}
